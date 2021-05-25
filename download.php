c75ce578af2b97b649106a9363ede91eae9ad9ee
Alpha <alpha@pulsarcode.com>
2021-05-25 11:00:24 +0200

    This is required because if not exists month fail when creating the day

--- download.php
+++ download.php
@@ -64,7 +64,7 @@ foreach ($download as $symbol)
 
             if (!file_exists($downloadDirectory))
             {
-                mkdir($downloadDirectory);
+                mkdir($downloadDirectory, 0777, true);
             }
         }
 
