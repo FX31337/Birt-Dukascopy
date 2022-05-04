98dae6845306a33373ebc11e2bbea71c495c0575
Alpha <alpha@pulsarcode.com>
2022-05-04 13:18:51 +0200

    Add error files when response is != 200|400 and retry again the failed file, remove skip method to ensure every file is check and downloaded and remove unnecessary skip mesages when file exists locally

--- download.php
+++ download.php
@@ -49,16 +49,38 @@ foreach ($download as $symbol)
     }
 
     $maxNotFoundFiles  = 100;
+    $maxErrorFiles     = 100;
     $maxEmptyFiles     = 100;
-    $downloadDirectory = '';
     $notFoundFiles     = 0;
+    $errorFiles        = 0;
     $emptyFiles        = 0;
-    $daysToSkip        = 1;
     $currentDateTime   = new \DateTime('now', new \DateTimeZone('UTC'));
+    $downloadDirectory = '';
     logger('Downloading ' . $symbol . '...');
 
-    while ($maxNotFoundFiles > $notFoundFiles && $maxEmptyFiles > $emptyFiles && 1970 < $currentDateTime->format('Y'))
+    while (1970 < $currentDateTime->format('Y'))
     {
+        if ($maxNotFoundFiles < $notFoundFiles)
+        {
+            logger('Too many not found files, aborting...');
+
+            break;
+        }
+
+        if ($maxErrorFiles < $errorFiles)
+        {
+            logger('Too many error files, aborting...');
+
+            break;
+        }
+
+        if ($maxEmptyFiles < $emptyFiles)
+        {
+            logger('Too many empty files, aborting...');
+
+            break;
+        }
+
         $currentDateTime->modify('-1 hour');
         $relativePath   = getRelativePath($symbol, $currentDateTime);
         $fileToDownload = __DIR__ . DIRECTORY_SEPARATOR . $relativePath;
@@ -74,103 +96,85 @@ foreach ($download as $symbol)
             }
         }
 
-        if (!is_file($fileToDownload))
+        if (is_file($fileToDownload))
         {
-            $curl    = false;
-            $retry   = 0;
-            $retries = 10;
+            continue;
+        }
 
-            do
+        $curl    = false;
+        $retry   = 0;
+        $retries = 10;
+
+        do
+        {
+            if (false !== $curl)
             {
-                if (false !== $curl)
-                {
-                    curl_close($curl);
-                }
-
-                $curl = curl_init($dukascopyUrl);
-                curl_setopt_array($curl, $curlOptions);
-                $result = curl_exec($curl);
-                $retry++;
-
-                if (1 < $retry)
-                {
-                    sleep(1);
-                }
-
-            }
-            while ($retry <= $retries && curl_errno($curl));
-
-            if (curl_errno($curl))
-            {
-                logger('Couldn\'t download ' . $dukascopyUrl);
-                logger('Error was: ' . curl_error($curl));
-
-                exit(1);
-            }
-            else
-            {
-                $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-                switch ($httpCode)
-                {
-                    case 404:
-                        $notFoundFiles++;
-                        logger($dukascopyUrl . ' Not Found');
-
-                        break;
-
-                    case 200:
-                        $notFoundFiles = 0;
-                        $emptyFiles    = empty($result) ? $emptyFiles + 1 : 0;
-
-                        if (saveBinary($fileToDownload, $result))
-                        {
-                            logger($dukascopyUrl . ' downloaded ' . mb_strlen($result, '8bit') . ' bytes');
-                        }
-                        else
-                        {
-                            logger('Couldn\'t open ' . $fileToDownload);
-
-                            exit(1);
-                        }
-
-                        break;
-
-                    default:
-                        logger('Error when downloading ' . $dukascopyUrl);
-                        logger('HTTP code was: ' . $httpCode);
-                        logger('Content was: ' . $result);
-
-                        break;
-                }
+                curl_close($curl);
             }
 
-            curl_close($curl);
+            $curl = curl_init($dukascopyUrl);
+            curl_setopt_array($curl, $curlOptions);
+            $result = curl_exec($curl);
+            $retry++;
+
+            if (1 < $retry)
+            {
+                sleep(1);
+            }
+
+        }
+        while ($retry <= $retries && curl_errno($curl));
+
+        if (curl_errno($curl))
+        {
+            logger('Couldn\'t download ' . $dukascopyUrl);
+            logger('Error was: ' . curl_error($curl));
+
+            exit(1);
         }
         else
         {
-            if (is_file('.done-' . $symbol))
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+            switch ($httpCode)
             {
-                logger('The symbol ' . $symbol . ' is already full downloaded, just downloaded the last new entries');
+                case 404:
+                    $notFoundFiles++;
+                    logger($dukascopyUrl . ' Not Found');
 
-                break;
+                    break;
+
+                case 200:
+                    $errorFiles    = 0;
+                    $notFoundFiles = 0;
+                    $emptyFiles    = empty($result) ? $emptyFiles + 1 : 0;
+
+                    if (saveBinary($fileToDownload, $result))
+                    {
+                        logger($dukascopyUrl . ' downloaded ' . mb_strlen($result, '8bit') . ' bytes');
+                    }
+                    else
+                    {
+                        logger('Couldn\'t open ' . $fileToDownload);
+
+                        exit(1);
+                    }
+
+                    break;
+
+                default:
+                    $errorFiles++;
+                    $currentDateTime->modify('+1 hour');
+                    logger('Error when downloading ' . $dukascopyUrl);
+                    logger('HTTP code was: ' . $httpCode);
+                    logger('Content was: ' . $result);
+
+                    break;
             }
-
-            $currentDateTimeToSkip = clone $currentDateTime;
-            $currentDateTimeToSkip->modify('-' . min(30, $daysToSkip) . ' day');
-            $relativePathToSkip   = getRelativePath($symbol, $currentDateTimeToSkip);
-            $fileToDownloadToSkip = __DIR__ . DIRECTORY_SEPARATOR . $relativePathToSkip;
-
-            if (is_file($fileToDownloadToSkip))
-            {
-                $daysToSkip++;
-                $currentDateTime = $currentDateTimeToSkip;
-            }
-
-            logger($dukascopyUrl . ' skipped, local file already exists');
         }
+
+        curl_close($curl);
     }
 
     logger('Downloading ' . $symbol . ' completed');
-    touch('.done-' . $symbol);
 }
